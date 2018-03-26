8b745eda97cf87230c027024fc3a7b072fbc055d
oshvorak <oshvorak@student.unit.ua>
2018-03-26 18:38:02 +0300

    main

--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   filler.h                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oshvorak <oshvorak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/10 17:40:03 by oshvorak          #+#    #+#             */
-/*   Updated: 2018/03/21 17:17:23 by oshvorak         ###   ########.fr       */
+/*   Created: 2018/03/26 18:22:23 by oshvorak          #+#    #+#             */
+/*   Updated: 2018/03/26 18:32:38 by oshvorak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLER_H
-# define FILLER_H
+#include "../inc/fdf.h"
 
-# include <fcntl.h>
-# include <stdlib.h>
-# include <errno.h>
-# include "../lib/libft.h"
-# include "../lib/get_next_line/get_next_line.h"
-# include "../lib/ft_printf/inc/ft_printf.h"
+int main(int argc, char **argv)
+{
+    int     fd;
+    char    *line;
 
-#endif
+    (argc == 2) ? fd = open(argv[1], O_RDONLY) : exit(1);
+    while (get_next_line(fd, line))
+    return (0);
+}
